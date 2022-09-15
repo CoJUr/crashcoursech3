de7547f551fdaf91acd8fd3c29fd4425bfa9c9eb
“Cory <coryurton@gmail.com>
2022-09-15 01:20:03 -0500

    up to verifying list not empty

--- ch5/5.3-5.7/alien_colors.py
+++ ch5/5.3-5.7/alien_colors.py
@@ -19,5 +19,60 @@ alien_color = 'red'
 
 if alien_color == 'green':
     print('You just earned 5 points for shooting down the alien!')
+elif alien_color == 'yellow':
+    print('You just got TEN POINTS!')
+elif alien_color == 'red':
+    print("15 WHOLE POINTS")
 else:
     print('+10 points!!')
+
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You just earned 5 points for shooting down the alien!')
+elif alien_color == 'yellow':
+    print('You just got TEN POINTS!')
+elif alien_color == 'red':
+    print("15 WHOLE POINTS")
+else:
+    print('+10 points!!')
+
+alien_color = 'brown'
+
+if alien_color == 'green':
+    print('You just earned 5 points for shooting down the alien!')
+elif alien_color == 'yellow':
+    print('You just got TEN POINTS!')
+elif alien_color == 'red':
+    print("15 WHOLE POINTS")
+else:
+    print('+10 points!!')
+
+age = 20
+
+if age < 2:
+    print("You just a baby")
+elif age < 4:
+    print("Toddler")
+elif age < 13:
+    print("Just a kiddo")
+elif age < 20:
+    print('I remember when I was a teen')
+elif age < 65:
+    print('Ah, adulthood')
+elif age <= 65:
+    print('The golden years')
+
+favorite_fruits = ['apricot', 'watermelon', 'grape']
+
+if 'apricot' in favorite_fruits:
+    print('Got the apricots')
+if 'watermelin' not in favorite_fruits:
+    print('Watermelin? Never heard of it. Did you mean watermelON?')
+if 'watermelon' in favorite_fruits:
+    print('Thought so!')
+if 'bananas' in favorite_fruits:
+    print('Bananas? I\'m bananas for bananas too!')
+if 'grape' in favorite_fruits:
+    print("Yes...should we make it 'grapes' though?")
+
--- ch5/toppings.py
+++ ch5/toppings.py
@@ -1,5 +1,21 @@
-requested_topping = 'mushrooms'
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
-if requested_topping != 'anchovies':
-    print('Hold the anchovies!')
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we ran out of green peppers.")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
+
+# checking that a list is not empty
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
+
 
